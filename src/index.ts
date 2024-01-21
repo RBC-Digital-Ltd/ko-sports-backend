0d6468a6f718871f8e392505848917460beb7998
Rahul Parkar <rahul.a.parkar@gmail.com>
2024-01-21 01:32:11 +0000

    Add sentry to Lambda

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
-export const handler = async (event: AWSLambda.APIGatewayProxyEvent) => {
-  return {
-    statusCode: 200,
-    headers: {
-      "x-api-version": process.env.API_VERSION,
-    },
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v3.0! Your function executed successfully!",
-        input: event,
+import { wrapHandler } from "./utils/wrapHandler";
+
+export const handler = wrapHandler(
+  async (event: AWSLambda.APIGatewayProxyEvent) => {
+    return {
+      statusCode: 200,
+      headers: {
+        "x-api-version": process.env.API_VERSION,
       },
-      null,
-      2,
-    ),
-  };
-};
+      body: JSON.stringify(
+        {
+          message: "Go Serverless v3.0! Your function executed successfully!",
+          input: event,
+        },
+        null,
+        2,
+      ),
+    };
+  },
+);
