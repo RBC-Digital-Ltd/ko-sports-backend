9e7a3711ac149c3e970fe210ac2f92b0569d3c0b
Rahul Parkar <rahul.a.parkar@gmail.com>
2024-01-21 00:57:32 +0000

    Install ESBuild to transpile TS

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-module.exports.handler = async (event: AWSLambda.APIGatewayProxyEvent) => {
+export const handler = async (event: AWSLambda.APIGatewayProxyEvent) => {
   return {
     statusCode: 200,
     headers: {
