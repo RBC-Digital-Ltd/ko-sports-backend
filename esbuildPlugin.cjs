7146af6c70d3dd199a6278827c66ba61ff6a2c2d
Rahul Parkar <rahul.a.parkar@gmail.com>
2024-01-27 22:30:29 +0000

    Stop logging env in esbuiild plugins

--- esbuildPlugin.cjs
+++ esbuildPlugin.cjs
@@ -1,8 +1,6 @@
 const { env } = require("node:process");
 const { sentryEsbuildPlugin } = require("@sentry/esbuild-plugin");
 
-console.log(env);
-
 let plugins = [
   sentryEsbuildPlugin({
     authToken: env.SENTRY_AUTH_TOKEN,
