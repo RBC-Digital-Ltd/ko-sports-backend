170adc9612f707346b2f7dace56e075a95f8ab1d
Rahul Parkar <rahul.a.parkar@gmail.com>
2024-04-20 03:33:53 +0100

    Update deprecated node profiling

--- src/utils/wrapHandler.ts
+++ src/utils/wrapHandler.ts
@@ -1,10 +1,10 @@
 import { type Handler } from "aws-lambda";
 import { AWSLambda } from "@sentry/serverless";
-import { ProfilingIntegration } from "@sentry/profiling-node";
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
 
 AWSLambda.init({
   dsn: "https://c904a50fae8b93c7347d6cec226b1d65@o369886.ingest.sentry.io/4506606652424192",
-  integrations: [new ProfilingIntegration()],
+  integrations: [nodeProfilingIntegration()],
   // Performance Monitoring
   tracesSampleRate: 1, //  Capture 100% of the transactions
   // Set sampling rate for profiling - this is relative to tracesSampleRate
