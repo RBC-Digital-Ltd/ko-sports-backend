4bb009e2d5413fd40a2a048072becab696211e27
Rahul Parkar <rahul.a.parkar@gmail.com>
2024-05-06 19:00:02 +0100

    Create Update Profile route

---
+++ src/routes/auth/updateProfile/index.ts
@@ -0,0 +1,20 @@
+import { sql } from "~/db";
+import { buildReturn } from "~/utils/return";
+import { wrapHandler } from "~/utils/wrapHandler";
+
+export const handler = wrapHandler(
+  async (event: AWSLambda.APIGatewayProxyEvent) => {
+    const body = JSON.parse(event.body || "{}");
+
+    const result = await sql`
+    UPDATE users SET
+    first_name = ${body.firstName},
+    last_name = ${body.lastName},
+    profile_complete = ${true}
+    WHERE auth0_id = ${event.requestContext.authorizer?.claims?.sub}
+     RETURNING *;
+  `;
+
+    return buildReturn(200, result[0]);
+  },
+);
