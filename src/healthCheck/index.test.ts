6f94e77e42a81106eef069dc4ada1ec7554bee2e
Rahul Parkar <rahul.a.parkar@gmail.com>
2024-06-22 23:51:18 +0100

    Switch to Jest

--- src/healthCheck/index.test.ts
+++ src/healthCheck/index.test.ts
@@ -1,23 +1,20 @@
 import { type Context } from "aws-lambda";
-import { test } from "@japa/runner";
 import { handler } from "./index";
 
-test.group("Health Check", () => {
-  test("should return 200 with the right message", async ({ expect }) => {
-    const response = await handler({}, {} as unknown as Context, () => {});
+test("should return 200 with the right message", async () => {
+  const response = await handler({}, {} as unknown as Context, () => {});
 
-    expect(response).toEqual({
-      statusCode: 200,
-      headers: {
-        "x-api-version": process.env.API_VERSION,
+  expect(response).toEqual({
+    statusCode: 200,
+    headers: {
+      "x-api-version": process.env.API_VERSION,
+    },
+    body: JSON.stringify(
+      {
+        message: "Health Check Successful",
       },
-      body: JSON.stringify(
-        {
-          message: "Health Check Successful",
-        },
-        undefined,
-        2,
-      ),
-    });
+      undefined,
+      2,
+    ),
   });
 });
