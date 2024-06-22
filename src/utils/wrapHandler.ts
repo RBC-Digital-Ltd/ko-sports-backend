8561771036cd598ea84d4d853baedd5db0bc570a
Rahul Parkar <rahul.a.parkar@gmail.com>
2024-06-23 00:06:32 +0100

    Update serverless sentry to right version

--- src/utils/wrapHandler.ts
+++ src/utils/wrapHandler.ts
@@ -1,10 +1,11 @@
 import { type Handler } from "aws-lambda";
-import { AWSLambda } from "@sentry/serverless";
+import Sentry from "@sentry/aws-serverless";
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
 
 if (process.env.NODE_ENV !== "test") {
-  AWSLambda.init({
+  Sentry.init({
     dsn: "https://c904a50fae8b93c7347d6cec226b1d65@o369886.ingest.sentry.io/4506606652424192",
-    integrations: [],
+    integrations: [nodeProfilingIntegration()],
     // Performance Monitoring
     tracesSampleRate: 1, //  Capture 100% of the transactions
     // Set sampling rate for profiling - this is relative to tracesSampleRate
@@ -17,5 +18,5 @@ export const wrapHandler = (handler: Handler) => {
     return handler;
   }
 
-  return AWSLambda.wrapHandler(handler);
+  return Sentry.wrapHandler(handler);
 };
