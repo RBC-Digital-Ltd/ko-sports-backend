b1c47a287dda4a4b314134728215f2fc8e743bb6
Rahul Parkar <rahul.a.parkar@gmail.com>
2024-08-07 22:54:29 +0100

    Use strict equal

--- src/utils/return.test.ts
+++ src/utils/return.test.ts
@@ -3,7 +3,7 @@ import { buildReturn } from "./return";
 describe("buildReturn", () => {
   it("should return a response with the correct status code and body", () => {
     const response = buildReturn(200, { message: "Hello, world!" });
-    expect(response).toEqual({
+    expect(response).toStrictEqual({
       statusCode: 200,
       headers: {
         "x-api-version": process.env.API_VERSION,
